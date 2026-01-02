Arata-Labs/device_xiaomi_surya

vendorsetup: Add KernelSU-Next check
Signed-off-by: HinohArata <mmgcntk@gmail.com>

vendorsetup.sh
@@ -6,3 +6,15 @@ if [ -f "$COMB" ]; then
     echo -e "- Executing ${COMB}"
     bash $COMB
 fi
+
+if [ -d kernel/xiaomi/surya ]; then
+    if [ -d kernel/xiaomi/surya/KernelSU-Next ]; then
+        if [ ! -d kernel/xiaomi/surya/KernelSU-Next/kernel ]; then
+            cd kernel/xiaomi/surya
+            git submodule init
+            git submodule update --recursive
+            rm -rf KernelSU-Next/userspace
+            cd ../../..
+        fi
+    fi
+fi

Haydn-Lab/device_xiaomi_haydn

vendorsetup.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+
+if [ -d kernel/xiaomi/sm8350 ]; then
+    if [ -d kernel/xiaomi/sm8350/KernelSU-Next ]; then
+        if [ ! -d kernel/xiaomi/sm8350/KernelSU-Next/kernel ]; then
+            cd kernel/xiaomi/sm8350
+            git submodule init
+            git submodule update --recursive
+            rm -rf KernelSU-Next/userspace
+            cd ../../..
+        fi
+    fi
+fi